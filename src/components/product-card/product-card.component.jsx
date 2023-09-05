72e7c47f416bf17d72de67610fa831f00600a76f
Bryan Lam <blam@ebsco.com>
2023-09-05 17:00:22 -0600

    Added in add to cart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
-import './product-card.styles.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 
+import './product-card.styles.scss';
+
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +19,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType="invert">Add to Cart</Button>
+            <Button buttonType="invert" onClick={addProductToCart}>Add to Cart</Button>
         </div>
     );
 }
